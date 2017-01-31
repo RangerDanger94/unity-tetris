cf3f040379a99552e687b1b966a97cc9377354d3
James Martin <James Martin>
2017-01-31 14:09:35 +1100

    Added menu and music using Unity live tutorials & sample assets

---
+++ Assets/Scripts/SelectOnInput.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectOnInput : MonoBehaviour {
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+    private bool buttonSelected;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetAxisRaw("Vertical") != 0 && !buttonSelected)
+        {
+            eventSystem.SetSelectedGameObject(selectedObject);
+            buttonSelected = true;
+        } else if ( (Input.GetAxisRaw("Mouse X") != 0 || Input.GetAxisRaw("Mouse Y") != 0) && buttonSelected )
+        {
+            eventSystem.SetSelectedGameObject(null);
+            buttonSelected = false;
+        }
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("Script was removed");
+        buttonSelected = false;
+    }
+}
---
+++ Assets/Scripts/VolumeControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.EventSystems;
+
+public class VolumeControl : MonoBehaviour {
+    public AudioMixer mixer;
+    public EventSystem events;
+
+	public void SetMasterVolume(float level)
+    {
+        mixer.SetFloat("MasterVolume", level);
+    }
+
+    public void SetMusicVolume(float level)
+    {
+        mixer.SetFloat("MusicVolume", level);
+    }
+
+    public void SetEffectsVolume(float level)
+    {
+        mixer.SetFloat("EffectsVolume", level);
+    }
+
+    
+}
